0f74b9ff478a1927b88763733a70ed3368323d1d
Andrii Zubrytskyi <andreizubritskiy@gmail.com>
2018-04-29 23:49:32 +0300

    Add ES6 modules support to the server and mongoose

--- server/server.js
+++ server/server.js
@@ -1,7 +1,18 @@
-const express = require('express');
+import express from 'express';
+import mongoose from 'mongoose';
+import serverConfig from './config';
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(serverConfig.mongoURL, (error) => {
+  if (error) {
+    console.error('Please make sure Mongodb is installed and running!');
+    throw error;
+  }
+});
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
