ded11fbe6e82d485a803f15b4ff28a928b2e70b5
Andrii Zubrytskyi <andreizubritskiy@gmail.com>
2018-05-06 00:31:12 +0300

    Add sorting of rooms list

--- client/src/app/universities/university-details/building-details/rooms-list/room-list.js
+++ client/src/app/universities/university-details/building-details/rooms-list/room-list.js
@@ -1,22 +1,26 @@
 import React from "react";
 import "./room-list.css";
+import _ from "lodash";
 
 class RoomList extends React.Component {
   render() {
     return (
       <ul className="rooms-list">
-        {this.props.rooms.map(room => {
-          return (
-            <li>
-              <div>
-                Аудиторія № {room.name}
-                <br />
-                Повне ім'я: {room["full_name"]}
-              </div>
-              <div className="button">Забронювати</div>
-            </li>
-          );
-        })}
+        {_(this.props.rooms)
+          .sortBy("name")
+          .map(room => {
+            return (
+              <li>
+                <div>
+                  Аудиторія № {room.name}
+                  <br />
+                  Повне ім'я: {room["full_name"]}
+                </div>
+                <div className="button">Забронювати</div>
+              </li>
+            );
+          })
+          .value()}
       </ul>
     );
   }
